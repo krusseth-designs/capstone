9586a2355198375e4d731afeebe0b083bf223262
Karen Russeth <66326669+krusseth-designs@users.noreply.github.com>
2024-02-20 18:02:47 -0500

    Add files via upload

---
+++ poke-game/frontend/src/PokeDex.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useAxios } from "./hooks";
+import PokemonCard from "./PokemonCard";
+import "./PokeDex.css";
+
+/* Renders a list of pokemon cards. 
+    This component uses the useAxios hook to get the list of pokemon from the postgreSQL database.
+    Refactored to use the useAxios hook to get the list of pokemon from the postgreSQL database.
+*/
+
+  const PokeDex = () => {
+    // Get the list of pokemon from the postgreSQL database using the useAxios hook
+    const [pokemon, addPokemon] = useAxios("pokemon", "https://pokeapi.co/api/v2/pokemon/");
+
+    return (
+     <div className="PokeDex">
+      <div className="PokeDex-card-area">
+        {pokemon.map(card => (
+          <PokemonCard key={card.id} {...card}/>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PokeDex;
---
+++ poke-game/frontend/src/PokemonCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useFlip } from "./hooks";
+import "./PokemonCard.css";
+
+/* Renders a single pokemon card. */
+/* Part 2 - Refactored to include logic from imported custom hook useFlip*/
+function PokemonCard({ front, back, name, stats }) {
+  const [isFacingUp, flip] = useFlip();
+  return (
+    <div onClick={flip} className="PokemonCard Card">
+      {isFacingUp ? (
+        <div className="PokemonCard-front">
+          <img src={front} alt={`{name} front`} />
+          <div>
+            <p className="PokemonCard-name">Name: {name}</p>
+            <p className="PokemonCard-stat">Type: {stats.type}</p>
+            <p className="PokemonCard-stat">EXP: {stats.base_experience}</p>
+            <p className="PokemonCard-stat">Attack: {stats.attack}</p>
+            <p className="PokemonCard-stat">Defense: {stats.defense}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="PokemonCard-back">
+          <img src={back} alt={`{name} back`} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PokemonCard;
+
+
+
+
+
+
